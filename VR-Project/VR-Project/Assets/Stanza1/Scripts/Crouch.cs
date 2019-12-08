ea9686d12bf4df54aa037b31a0f5f6e23b1c1f29
Ari-bit <56387993+Ari-bit@users.noreply.github.com>
2019-12-08 18:11:27 +0100

    ci proviamo

---
+++ VR-Project/VR-Project/Assets/Stanza1/Scripts/Crouch.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crouch : MonoBehaviour {
+
+    //=================================================================================
+    // © 2016 Scott Durkin, All rights reserved.
+    // By Downloading and using this script credit must 
+    // be given to the creator know as "Unity3D With Scott".
+    // YouTube Channel: https://www.youtube.com/channel/UC9hfBvn17qSIrdFwAk56oZg
+    //=================================================================================
+
+    public CharacterController characterController;
+
+    void Start ()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            characterController.height = 0.5f;
+        }
+        else
+        {
+            characterController.height = 1.8f;
+        }
+    }
+}
