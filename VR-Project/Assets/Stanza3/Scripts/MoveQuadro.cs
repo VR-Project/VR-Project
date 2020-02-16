ac50b8da13aaef65d6f3dc43cb84a1e27d57d3dc
Giorgia Marullo <giorgia_marullo96@hotmail.it>
2020-02-16 10:00:17 +0100

    Move quadro

--- VR-Project/Assets/Stanza3/Scripts/MoveQuadro.cs
+++ VR-Project/Assets/Stanza3/Scripts/MoveQuadro.cs
@@ -18,19 +18,20 @@ public class MoveQuadro : MonoBehaviour
         
     }
 
-    public IEnumerable MoveQ()
+    public IEnumerator MoveQ()
     {
-        while(pos < 1f)
+        while(pos < 0.8f)
         {
             pos +=  0.01f;
-            gameObject.transform.Translate(0, 0, -0.005f);
-            yield return new WaitForSeconds(.05f);
+            gameObject.transform.Translate(0, -0.01f, 0);
+            yield return new WaitForSeconds(.02f);
         }
+        yield return new WaitForSeconds(0.3f);
         while (pos > 0)
         {
             pos -= 0.01f;
-            gameObject.transform.Translate(0, 0, +0.005f);
-            yield return new WaitForSeconds(.05f);
+            gameObject.transform.Translate(0, +0.01f, 0);
+            yield return new WaitForSeconds(.007f);
         }
 
     }
