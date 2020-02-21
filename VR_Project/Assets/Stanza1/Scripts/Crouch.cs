14b6e7d050f53829ddb4b5580c072306113ebb0b
Giorgia Marullo <giorgia_marullo96@hotmail.it>
2020-02-21 10:42:07 +0100

    Prima consegna

---
+++ VR_Project/Assets/Stanza1/Scripts/Crouch.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Crouch : MonoBehaviour {
+
+    public CharacterController characterController;
+    public bool isCrouched = false;
+    private bool guardaSotto = false;
+    [SerializeField] private Image Striscia;
+
+    void Start ()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();        
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl) && isCrouched == false)
+        {
+            Striscia.enabled = true;
+            characterController.height = 0.3f;
+            isCrouched = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftControl) && isCrouched==true)
+        {
+            if(guardaSotto == true) transform.localScale += new Vector3(0.0f, 0.8f, 0);
+            
+            characterController.height = 1.8f;
+            isCrouched = false;
+            guardaSotto = false;
+            Striscia.enabled = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightControl) && isCrouched == true && guardaSotto == false)
+        {
+            guardaSotto = true;
+            transform.localScale += new Vector3(0.0f, -0.8f, 0);
+            Striscia.enabled = false;
+        }
+    }
+}
